60e85366c9f693912bccd10beb7421b94083b652
Jany <kuba-janys@seznam.cz>
2018-10-19 19:49:02 +0200

    Menu component

---
+++ frontend/src/components/Menu.tsx
@@ -0,0 +1,52 @@
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+import * as PropTypes from 'prop-types';
+import * as React from 'react';
+
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+    root: {
+        flexGrow: 1,
+    },
+};
+
+function ButtonAppBar(props: any) {
+    const { classes } = props;
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" color="inherit" className={classes.grow}>
+                        HELPDESK
+          </Typography>
+                    <Button color="inherit">Menu item</Button>
+                    <Button color="inherit">Menu item</Button>
+                    <Button color="inherit">Menu item</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
+}
+
+ButtonAppBar.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ButtonAppBar);
