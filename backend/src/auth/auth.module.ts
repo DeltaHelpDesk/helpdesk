ad15311f9751f7bda6b0ca662c8b439701d5110d
botaifilip <botaifilip@gmail.com>
2018-11-20 08:56:22 +0100

    its working

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,13 +3,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([User]),
         PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.register({
+            secretOrPrivateKey: `bi',.;p[p[../42y5u6jugng259/**[;l;'.;'joigytyg215985+39+*-*9+393gtryerttyrweswry8tbd];`,
+            signOptions: {
+                expiresIn: 3600,
+            },
+        }),
     ],
-    providers: [AuthService],
+    providers: [
+        JwtStrategy,
+        AuthService,
+    ],
     controllers: [],
 })
 export class AuthModule {
