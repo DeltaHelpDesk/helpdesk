cca01278a1afc2938f5f29f660921a090005d6f7
Jany <kuba-janys@seznam.cz>
2018-11-20 14:57:59 +0100

    /login-component: Auto stash before merge of "feature/login-component" and "origin/dev"

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -1,18 +1,19 @@
-
 import * as classNames from "classnames";
-import { withStyles } from "@material-ui/core/styles";
+import { withStyles, Theme } from "@material-ui/core/styles";
 import IconButton from "@material-ui/core/IconButton";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import TextField from "@material-ui/core/TextField";
 import Visibility from "@material-ui/icons/Visibility";
 import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import Button from "@material-ui/core/Button";
+import { WithStyles } from "@material-ui/core";
 
 import * as React from "react";
 
+
 // TODO: type definitons
 
-const styles = (theme: any) => ({
+const styles = (theme: Theme) => ({
     root: {
         display: "flex",
         flexWrap: "wrap"
@@ -28,9 +29,15 @@ const styles = (theme: any) => ({
     }
 });
 
+interface FilledInputAdornmentsState {
+    name: string,
+    password: string,
+    showPassword: boolean
+}
 
+type FilledInputAdornmentsProps<T> = WithStyles<T> & Record<"mode", boolean>
 
-class FilledInputAdornments extends React.Component<any, any> {
+class FilledInputAdornments extends React.Component<FilledInputAdornmentsProps<typeof styles>, FilledInputAdornmentsState> {
 
     state = {
         name: "",
@@ -38,16 +45,17 @@ class FilledInputAdornments extends React.Component<any, any> {
         showPassword: false
     };
 
-    handleChange = (prop: any) => (event: any) => {
-        this.setState({ [prop]: event.target.value });
+    handleChange = (prop: any) => (event: ) => {
+        this.setState({ [prop]: eventtarget.value });
     };
 
     handleClickShowPassword = () => {
-        this.setState((state: any) => ({ showPassword: !state.showPassword }));
+        this.setState((state) => ({ showPassword: !state.showPassword }));
     };
 
     render() {
-        const classes: any = this.props;
+        const { classes } = this.props;
+
 
         return (
             <div className={classes.root}>
@@ -91,4 +99,4 @@ class FilledInputAdornments extends React.Component<any, any> {
 }
 
 
-export default withStyles(styles as any)(FilledInputAdornments);
+export default withStyles(styles)(FilledInputAdornments);
