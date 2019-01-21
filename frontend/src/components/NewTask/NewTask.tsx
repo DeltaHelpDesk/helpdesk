cad2b88e8b000b479921074234903af3bdf3359e
Dominik <dominikslechta.1@gmail.com>
2019-01-21 07:33:39 +0100

    new task fix

--- frontend/src/components/NewTask/NewTask.tsx
+++ frontend/src/components/NewTask/NewTask.tsx
@@ -1,9 +1,9 @@
-import { withStyles, Theme } from "@material-ui/core/styles";
+﻿import { withStyles, Theme } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import { WithStyles } from "@material-ui/core";
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Input from '@material-ui/core/Input';
 
 import * as React from "react";
 
@@ -24,46 +24,44 @@ const styles = (theme: Theme) => ({
 });
 
 
-
-interface IFilledInputAdornmentsState {
-    task: INewTask
-}
-
 interface INewTask {
     subject: string,
     issue: string,
     assigne: string,
 }
 
-type FilledInputAdornmentsProps<T> = WithStyles<string> & Record<"mode", boolean>
+class FilledInputAdornments extends React.Component<INewTask>{
+    constructor(props: INewTask) {
+        super(props)
+        this.state = {
+            subject: " ",
+            issue: " ",
+            assigne: " ",
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-class FilledInputAdornments extends React.Component<FilledInputAdornmentsProps<typeof styles>, IFilledInputAdornmentsState> {
-    NewTask = {
-        task: {
-            subject: "",
-            issue: "",
-            assigne: ""
-        },
-    };
 
-    handleInputChange(e: React.FormEvent<HTMLInputElement>) {
+    handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
         const property = e.currentTarget.name;
         const value = e.currentTarget.value;
 
-        this.NewTask.task[property] = value;
+        this.setState({
+            [property]: value
+        })
     };
-
-
-    handleChange = (event: any)=> {
-        this.NewTask.task[2] = event.target.value;
-      };
-
-    handleSubmit = () => {
-        console.log(this.NewTask.task)
+    handleSelectedEvent(e: React.ChangeEvent<HTMLSelectElement>) {
+        this.setState({
+            assigne: e.target.value
+        })
+    };
+    handleSubmit() {
+        console.log(this.state)
     }
 
     render() {
-        const { classes } = this.props;
+	const { classes } = this.props;
         return (
             <div className={classes.marginCenter}>
                 <form className={classes.root}>
@@ -71,40 +69,41 @@ class FilledInputAdornments extends React.Component<FilledInputAdornmentsProps<t
                         id="filled-adornment-subject"
                         variant="filled"
                         name="subject"
-                        className={classes.items}
+className={classes.items}
                         label="Subject"
                         type="text"
-                        value={this.NewTask.task.subject}
-                        onChange={e => this.handleInputChange(e as React.FormEvent<HTMLInputElement>)}
+                        value={this.props.subject}
+                        onChange={(e) => this.handleInputChange(e as React.ChangeEvent<HTMLInputElement>)}
                     />
                     <TextField
                         id="filled-adornment-issue"
                         variant="filled"
                         name="issue"
                         label="Issue"
-                        className={classes.items}
+className={classes.items}
                         type="text"
-                        value={this.NewTask.task.issue}
-                        onChange={e => this.handleInputChange(e as React.FormEvent<HTMLInputElement>)}
+                        value={this.props.issue}
+                        onChange={(e) => this.handleInputChange(e as React.ChangeEvent<HTMLInputElement>)}
                     />
                     <Select
-                        id="filled-adornment-issue"
-                        name="assigne"
-                        className={classes.items}
-                        variant="filled"
-                        onChange={e => this.handleChange(e)}
+                        onChange={(e) => this.handleSelectedEvent(e as React.ChangeEvent<HTMLSelectElement>)}
+                        input={<Input name="assingne" id="assingne-label" value={this.props.assigne} />}
+                        name="assingne"
+className={classes.items}
                     >
-                        
+                        <MenuItem value="">
+                            <em>None</em>
+                        </MenuItem>
                         <MenuItem value={10}>Ten</MenuItem>
                         <MenuItem value={20}>Twenty</MenuItem>
                         <MenuItem value={30}>Thirty</MenuItem>
                     </Select>
                     <Button
                         variant="contained"
-                        className={classes.items}
                         onClick={this.handleSubmit}
+className={classes.items}
                     >
-                        Přihlásit
+                        Přidat
                     </Button>
                 </form>
             </div>
