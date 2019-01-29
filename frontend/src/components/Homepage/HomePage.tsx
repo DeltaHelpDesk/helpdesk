8e1816e1b46735b06a49c37f1ce6267b805fd7db
tomaselsnic <tomaselsnic@seznam.cz>
2019-01-29 08:46:16 +0100

    Homepage restyled/frontend

--- frontend/src/components/Homepage/HomePage.tsx
+++ frontend/src/components/Homepage/HomePage.tsx
@@ -1,17 +1,17 @@
 import * as React from "react";
 import { Link } from 'react-router-dom';
-
+import Button from "@material-ui/core/Button";
 
 class HomePage extends React.Component {
     render() {
         return <div>
             <h1>DELTA HELPDESK</h1>
-            <h2>Vítejte na stránkách podpory</h2>
+            <h2>Vítejte na stránkách podpory :)</h2>
             <p>Pokud máte problém zašlete požadavek</p>
-            <Link to="/form">
-            <p>Poslat požadavek</p>
+            <Link to="/form"> 
+            <Button variant="contained" color="primary" className="bluebutton" >Poslat požadavek</Button>
             </Link>
         </div>;
     }
 }
-export default HomePage;
+export default HomePage; 
