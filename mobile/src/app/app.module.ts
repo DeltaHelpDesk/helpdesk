c08359d31f1f563bd3c084e3889f5ae9fda4633d
zapotocnylubos <zapotocnylubos@gmail.com>
2019-01-29 08:40:56 +0100

    mobile v4 | added graphql module

--- mobile/src/app/app.module.ts
+++ mobile/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { GraphQLModule } from "./graphql.module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +16,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule
   ],
   providers: [
     StatusBar,
