1d6ebda437c7d998056bdede50dadcfaa979b10f
botaifilip <botaifilip@gmail.com>
2019-01-29 11:21:38 +0100

    change task state fixes, delete task fixes

--- backend/src/task/task.service.ts
+++ backend/src/task/task.service.ts
@@ -34,15 +34,24 @@ export class TaskService {
         if(!task) {
             throw new HttpException(`Task with id: ${taskId} not found`, HttpStatus.NOT_FOUND);
         }
+        if(!comment && !state && !assigneeId) {
+            throw new HttpException(`Haven't passed any changes for task`, HttpStatus.BAD_REQUEST);
+        }
+        let log = this.logRepository.create({ author, comment, task, state });
         if (state) {
             task.state = state;
         }
         if (assigneeId) {
             const assignee = await this.userRepository.findOne(assigneeId);
-            task.assignee = assignee;
+            if(assignee) {
+                task.assignee = assignee;
+                assignee.assignedTasks.push(task);
+                log.assignee = assignee;
+            } else {
+                throw new HttpException(`Assignee for task with id: ${assigneeId} not found`, HttpStatus.NOT_FOUND);
+            }
         }
         task = await this.taskRepository.save(task);
-        let log = this.logRepository.create({ author, comment, task });
         log = await this.logRepository.save(log);
         task.logs.push(log);
         return task;
@@ -50,7 +59,7 @@ export class TaskService {
     async deleteTask(taskId: number) {
         const task = await this.taskRepository.findOne(taskId);
         if (!task) {
-            return false;
+            throw new HttpException(`Task with id: ${taskId} not found`, HttpStatus.NOT_FOUND);
         }
         await this.taskRepository.delete(taskId);
         return true;
