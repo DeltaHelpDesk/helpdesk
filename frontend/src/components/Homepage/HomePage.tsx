acd96e91a308bdd2a6d52955d95ee5828c8b18a3
Tomáš Faltejsek <faltejsek9@gmail.com>
2019-02-10 12:25:38 +0100

    fix

--- frontend/src/components/Homepage/HomePage.tsx
+++ frontend/src/components/Homepage/HomePage.tsx
@@ -1,17 +1,23 @@
 import * as React from "react";
-import { Link } from 'react-router-dom';
+import { withRouter, RouteComponentProps } from 'react-router-dom';
 import Button from "@material-ui/core/Button";
 
-class HomePage extends React.Component {
+class HomePage extends React.Component<RouteComponentProps> {
+    redirectToForm = () => {
+        this.props.history.push("/form");
+    }
+    redirectToLogin = () => {
+        this.props.history.push("/login");
+    }
     render() {
         return <div>
             <h1>DELTA HELPDESK</h1>
             <h2>Vítejte na stránkách podpory :)</h2>
             <p>Pokud máte problém zašlete požadavek</p>
-            <Link to="/form"> 
-            <Button variant="contained" color="primary" className="bluebutton" >Poslat požadavek</Button>
-            </Link>
+            <Button onClick={this.redirectToForm} variant="contained" color="primary" className="bluebutton" >Poslat požadavek</Button>
+            <p>nebo</p>
+            <Button onClick={this.redirectToLogin} variant="contained" color="primary" className="bluebutton" >Přihlásit se</Button>
         </div>;
     }
 }
-export default HomePage; 
+export default withRouter(HomePage); 
