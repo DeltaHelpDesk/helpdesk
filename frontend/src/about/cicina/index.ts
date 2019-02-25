3caf33486a9fbd28f3094dbd8bc3f3335de086dd
tomas <t>
2019-02-25 20:19:58 +0100

    Elsnic-about add
    no

---
+++ frontend/src/about/cicina/index.ts
@@ -0,0 +1,8 @@
+import image from './image.jpg';
+
+const markdown = `
+## Tomáš Cicina Elsnic
+Ahoj ahoj ahoj tady tomáš pracoval jsem s teamem na frontendu toho projektu tak doufám že si ti libí a pokud ne tak máš smůlu smileyface.
+`;
+
+export default { image, markdown };
--- frontend/src/about/index.ts
+++ frontend/src/about/index.ts
@@ -1,3 +1,4 @@
 // export { default as nazevSlozky } from './nazevSlozky'
 
 export { default as botafi } from './botafi';
+export {default as cicina} from "./cicina";
