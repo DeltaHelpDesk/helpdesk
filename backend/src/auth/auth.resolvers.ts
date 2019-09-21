5ace584784a9d550e75c07daeb3fe320553d69fb
Jiri Falta <jirkafalta@gmail.com>
2019-09-21 13:08:21 +0200

    Permissions fix

--- backend/src/auth/auth.resolvers.ts
+++ backend/src/auth/auth.resolvers.ts
@@ -23,7 +23,7 @@ export class AuthResolvers {
         return await this.authService.getAdmins();
     }
 
-    @UseGuards(GqlAuthGuard, new GqlRoleGuard(UserRole.SUPERADMIN))
+    @UseGuards(GqlAuthGuard, new GqlRoleGuard(UserRole.ADMIN))
     @Query('users')
     async getUsers(): Promise<UserEntity[]> {
         return await this.authService.getUsers();
