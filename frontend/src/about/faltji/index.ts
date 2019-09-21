c349597227b0e271395520e2d24e63d736a1ccb3
Jiri Falta <jirkafalta@gmail.com>
2019-09-21 21:01:09 +0200

    Another image added

---
+++ frontend/src/about/faltji/index.ts
@@ -0,0 +1,8 @@
+import image from './image.png';
+
+const markdown = `
+## Jiří "Fjarik" Falta
+Na obrázku - grafický bůh Zerzi
+`;
+
+export default { image, markdown };
--- frontend/src/about/index.ts
+++ frontend/src/about/index.ts
@@ -1,12 +1,13 @@
 // export { default as nazevSlozky } from './nazevSlozky'
 
 export { default as botafi } from './botafi';
-export {default as cicina} from "./cicina";
-export {default as slavma} from "./slavma";
-export {default as jaina} from "./jaina";
-export {default as pepino} from "./pepino";
-export {default as dominik} from "./dominik";
-export {default as falty} from "./falty";
-export {default as janyja} from "./janyja";
-export {default as slamda} from "./slamda";
-export {default as stermi} from "./stermi";
+export { default as cicina } from "./cicina";
+export { default as slavma } from "./slavma";
+export { default as jaina } from "./jaina";
+export { default as pepino } from "./pepino";
+export { default as dominik } from "./dominik";
+export { default as falty } from "./falty";
+export { default as janyja } from "./janyja";
+export { default as slamda } from "./slamda";
+export { default as stermi } from "./stermi";
+export { default as faltji } from './faltji'
