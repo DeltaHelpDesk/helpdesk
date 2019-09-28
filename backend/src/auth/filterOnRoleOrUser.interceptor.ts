28e1ec8105836903cf627af41606aa0399143838
Jiri Falta <jirkafalta@gmail.com>
2019-09-28 11:29:48 +0200

    NestJS 6
    NestJS upgraded to version 6

--- backend/src/auth/filterOnRoleOrUser.interceptor.ts
+++ backend/src/auth/filterOnRoleOrUser.interceptor.ts
@@ -1,4 +1,4 @@
-import { Injectable, NestInterceptor, ExecutionContext } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { UserRole, checkUserRole } from './userRole.enum';
@@ -7,43 +7,45 @@ import { User } from './user.entity';
 import { pickBy } from 'lodash';
 
 @Injectable()
-export class FilterOnRoleOrUserInterceptor<T extends object> implements NestInterceptor<T | Array<T>, Partial<T> | Array<Partial<T>> > {
-    constructor(private filteredKeys: string[], private role?: UserRole | undefined, private userIdKey?: keyof T | undefined) {}
+export class FilterOnRoleOrUserInterceptor<T extends object> implements NestInterceptor<T | Array<T>, Partial<T> | Array<Partial<T>>> {
+    constructor(private filteredKeys: string[], private role?: UserRole | undefined, private userIdKey?: keyof T | undefined) { }
     intercept(
         context: ExecutionContext,
-        call$: Observable<T | Array<T>>,
-    ): Observable< Partial<T> | Array<Partial<T>> > {
+        next: CallHandler,
+    ): Observable<Partial<T> | Array<Partial<T>>> {
         // get graphql context and user from it
         const ctx = GqlExecutionContext.create(context);
         const user: User = ctx.getContext().req.user;
 
         // intercept every response in observable and filter it
-        return call$.pipe(map(data => {
-            // declare filtering function
-            const doFilter = (dataToFilter: T): Partial<T> | T => {
-                let shouldFilter = true;
-                if (!this.role && !this.userIdKey) {
-                    shouldFilter = false;
-                } else if (this.userIdKey && Number(dataToFilter[this.userIdKey]) !== Number(user.id)) {
-                    shouldFilter = false;
-                } else if (this.role && checkUserRole((user as User).role, this.role)) {
-                    shouldFilter = false;
-                }
+        return next
+            .handle()
+            .pipe(map(data => {
+                // declare filtering function
+                const doFilter = (dataToFilter: T): Partial<T> | T => {
+                    let shouldFilter = true;
+                    if (!this.role && !this.userIdKey) {
+                        shouldFilter = false;
+                    } else if (this.userIdKey && Number(dataToFilter[this.userIdKey]) !== Number(user.id)) {
+                        shouldFilter = false;
+                    } else if (this.role && checkUserRole((user as User).role, this.role)) {
+                        shouldFilter = false;
+                    }
 
-                if  (shouldFilter) {
-                    return pickBy(dataToFilter, (_, key) => !this.filteredKeys.includes(key));
+                    if (shouldFilter) {
+                        return pickBy(dataToFilter, (_, key) => !this.filteredKeys.includes(key));
+                    } else {
+                        return dataToFilter;
+                    }
+                };
+                // filter the object in response or filter array of objects in response
+                let filteredData: (Partial<T> | T) | Array<Partial<T> | T>;
+                if (Array.isArray(data)) {
+                    filteredData = data.map(doFilter) as T[];
                 } else {
-                    return dataToFilter;
+                    filteredData = doFilter(data);
                 }
-            };
-            // filter the object in response or filter array of objects in response
-            let filteredData: (Partial<T> | T) | Array<Partial<T> | T>;
-            if (Array.isArray(data)) {
-                filteredData = data.map(doFilter) as T[];
-            } else {
-                filteredData = doFilter(data);
-            }
-            return filteredData;
-        }));
+                return filteredData;
+            }));
     }
 }
