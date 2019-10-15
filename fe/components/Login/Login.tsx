7603d5a217ef1dd8dcbc59379375c85389cffdf3
Jiri Falta <jirkafalta@gmail.com>
2019-10-15 15:15:06 +0200

    Added login page

--- fe/components/Login/Login.tsx
+++ fe/components/Login/Login.tsx
@@ -7,11 +7,10 @@ import Button from "@material-ui/core/Button";
 import Icon from '@mdi/react';
 import Grid from "@material-ui/core/Grid";
 import { mdiLogin } from '@mdi/js'
-
 import * as React from "react";
-import MicrosoftButtonLogin from './MicrosoftButtonLogin';
-import { RouteComponentProps } from 'react-router-dom';
+// import MicrosoftButtonLogin from './MicrosoftButtonLogin';
 import { ReactAuthContext } from '../../src/graphql/auth';
+import { useState } from "react";
 
 
 
@@ -28,40 +27,33 @@ interface IUser {
 
 // type FilledInputAdornmentsProps<T> = WithStyles<string> & Record<"mode", boolean>
 
-class FilledInputAdornments extends React.Component<RouteComponentProps<any>, IFilledInputAdornmentsState> {
-    static contextType = ReactAuthContext;
-    state = {
-        user: {
-            name: "",
-            password: "",
-        },
-        showPassword: false
-    };
+const LoginPage: React.FunctionComponent<IFilledInputAdornmentsState> = ({ showPassword, user }) => {
+    const contextType = ReactAuthContext;
 
+    const [showPwd, setShowPwd] = useState<boolean>(showPassword);
 
-    handleInputChange(e: React.FormEvent<HTMLInputElement>) {
+    const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
         const property = e.currentTarget.name;
         const value = e.currentTarget.value;
 
-        this.setState(previousState => ({
-            user: {
-                ...previousState.user,
-                [property]: value
-            }
-        }));
+        // this.setState(previousState => ({
+        //     user: {
+        //         ...previousState.user,
+        //         [property]: value
+        //     }
+        // }));
     };
 
 
-    handleClickShowPassword = () => {
-        this.setState((state) => ({ showPassword: !state.showPassword }));
+    const handleClickShowPassword = () => {
+        setShowPwd(!showPwd);
     };
 
-    handleFormSubmit = async () => {
-        const { user } = this.state;
+    const handleFormSubmit = async () => {
         const { loginByEmail } = this.context;
         try {
             await loginByEmail(user.name, user.password);
-            this.props.history.push('/admin');
+            //this.props.history.push('/admin');
         } catch (e) {
             if (e && e.graphQLErrors && e.graphQLErrors[0]) {
                 alert(e.graphQLErrors[0].message); // TODO: material ui dialog
@@ -71,7 +63,7 @@ class FilledInputAdornments extends React.Component<RouteComponentProps<any>, IF
         }
     }
 
-    handleOfficeLogin = async () => {
+   /*const handleOfficeLogin = async () => {
         const { doLoginOffice } = this.context;
         try {
             await doLoginOffice();
@@ -83,87 +75,75 @@ class FilledInputAdornments extends React.Component<RouteComponentProps<any>, IF
                 console.error("handle login error", e);
             }
         }
-    }
+    }*/
 
-    handleKeywordKeyPress = (e: any) => {
+    const handleKeywordKeyPress = (e: any) => {
         if (e.key === 'Enter') {
             this.handleFormSubmit();
         }
     };
 
-    render() {
+    return (
+        <div >
+            <Grid container={true} direction="row" justify="center" alignItems="center">
+                <form >
+                    <Grid item={true}>
+                        <h2 >Login form</h2>
+                        <TextField
+                            id="name"
+                            variant="filled"
+                            name="name"
+                            label="Zadej jméno"
+                            type="text"
+                            onChange={e => handleInputChange(e as React.FormEvent<HTMLInputElement>)} />
+                    </Grid>
+                    <Grid item={true}>
+                        <TextField
+                            id="filled-adornment-password"
+                            variant="filled"
+                            name="password"
+                            type={showPassword ? "text" : "password"}
+                            label="Heslo"
+                            onChange={e => handleInputChange(e as React.FormEvent<HTMLInputElement>)}
+                            onKeyPress={handleKeywordKeyPress}
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment variant="filled" position="end">
+                                        <IconButton
+                                            aria-label="Toggle password visibility"
+                                            onClick={handleClickShowPassword}
+                                        >
+                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                        </IconButton>
+                                    </InputAdornment>
+                                )
+                            }}
+                        />
+                    </Grid>
+                    <Grid item={true}>
+                        <div >
+                            <Button
+                                variant="contained"
+                                size="large"
+                                color="primary"
+                                onClick={handleFormSubmit}>
+                                <Icon path={mdiLogin}
+                                    size={1}
+                                    color="white"
+                                />
+                                Přihlásit
+                        </Button>
+                        </div>
+                    </Grid>
+                    <Grid item={true}>
+                        <div >
+                            {/* <MicrosoftButtonLogin onClick={handleOfficeLogin} /> */}
+                        </div>
+                    </Grid>
+                </form>
+            </Grid>
+        </div>
+    );
+};
 
-
-
-        return (
-
-            <div >
-                <Grid container={true} direction="row" justify="center" alignItems="center">
-                    <form >
-                        <Grid item={true}>
-                            <h2 >Login form</h2>
-                            <TextField
-                                id="name"
-
-                                variant="filled"
-                                name="name"
-                                label="Zadej jméno"
-                                type="text"
-                                onChange={e => this.handleInputChange(e as React.FormEvent<HTMLInputElement>)} />
-                        </Grid>
-                        <Grid item={true}>
-                            <TextField
-                                id="filled-adornment-password"
-
-                                variant="filled"
-                                name="password"
-                                type={this.state.showPassword ? "text" : "password"}
-                                label="Heslo"
-                                onChange={e => this.handleInputChange(e as React.FormEvent<HTMLInputElement>)}
-                                onKeyPress={this.handleKeywordKeyPress}
-                                InputProps={{
-                                    endAdornment: (
-                                        <InputAdornment variant="filled" position="end">
-                                            <IconButton
-                                                aria-label="Toggle password visibility"
-                                                onClick={this.handleClickShowPassword}
-                                            >
-                                                {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
-                                            </IconButton>
-                                        </InputAdornment>
-                                    )
-                                }}
-                            />
-                        </Grid>
-                        <Grid item={true}>
-                            <div >
-                                <Button
-                                    variant="contained"
-                                    size="large"
-                                    color="primary"
-
-                                    onClick={this.handleFormSubmit}>
-                                    <Icon path={mdiLogin}
-                                        size={1}
-                                        color="white"
-                                    />
-                                    Přihlásit
-                            </Button>
-                            </div>
-                        </Grid>
-                        <Grid item={true}>
-                            <div >
-                                <MicrosoftButtonLogin onClick={this.handleOfficeLogin} />
-                            </div>
-                        </Grid>
-                    </form>
-                </Grid>
-            </div>
-
-        );
-    }
-}
-
-FilledInputAdornments.contextType = ReactAuthContext;
-
-export default FilledInputAdornments;
+export default LoginPage;
---
+++ fe/pages/login.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import Link from 'next/link';
+import Layout from '../components/Layouts/Layout';
+import { NextPage } from 'next';
+import LoginPage from '../components/Login/Login';
+
+const IndexPage: NextPage = () => {
+  return (
+    <Layout title="Home | HelpDesk">
+      <LoginPage showPassword={false} user={null} />
+    </Layout>
+  )
+}
+
+export default IndexPage
