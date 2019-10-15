962b7fbae99177333bd88bfb8c981692aad98e53
Karel Koudelka <k.koudelka01@gmail.com>
2019-10-15 14:52:09 +0200

    Head updated

--- fe/components/Layouts/Layout.tsx
+++ fe/components/Layouts/Layout.tsx
@@ -4,26 +4,29 @@ import Head from 'next/head'
 import MainAppBar from '../MainAppBar/MainAppBar'
 
 type Props = {
-  title?: string
+    title?: string
 }
 
 const Layout: React.FunctionComponent<Props> = ({
-  children,
-  title = 'Helpdesk',
+    children,
+    title = 'Helpdesk',
 }) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-    </Head>
-    <header>
-      <MainAppBar/>
-    </header>
-    {children}
-  </div>
-)
+        <div>
+            <Head>
+                <title>{title} - Helpdesk</title>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+                <link rel="stylesheet" href="static/static.css" />
+                <link rel="stylesheet" href="static/bootstrap.css" />
+                <link rel="stylesheet" href="static/mdb.css" />
+            </Head>
+            <header>
+                <MainAppBar />
+            </header>
+            {children}
+        </div>
+    )
 
 export default Layout;
