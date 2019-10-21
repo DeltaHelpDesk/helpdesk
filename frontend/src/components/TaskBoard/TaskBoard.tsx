9d9601cef2bd96437221f153ab9a865315d050ad
Jan Kollert <koller.honzik@gmail.com>
2019-10-21 21:40:23 +0200

    Trello update

--- frontend/src/components/TaskBoard/TaskBoard.tsx
+++ frontend/src/components/TaskBoard/TaskBoard.tsx
@@ -1,58 +1,41 @@
 import * as React from 'react';
-import { Query } from 'react-apollo';
-import { GET_TASKS } from '../TaskList/TaskListQueries';
-import Loading from '../Loading/Loading';
-import { Grid, Paper, Divider, Typography } from '@material-ui/core';
-import TaskCard from './TaskCard';
-import { ITask, State } from 'src/graphql/types';
+import Board from 'react-trello'
 
 
 
 class TaskBoard extends React.Component<{}> {
 
+    tasksData = {
+        lanes: [
+            {
+                id: 'uncompleted',
+                title: 'Nezapočato',
+                label: '2/2',
+                cards: [
+                    { id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', draggable: false },
+                    { id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: { sha: 'be312a1' } }
+                ]
+            },
+            {
+                id: 'inprogress',
+                title: 'Pracuje se na tom',
+                label: '0/0',
+                cards: []
+            },
+            {
+                id: 'completed',
+                title: 'Dokončeno',
+                label: '0/0',
+                cards: []
+            }
+        ]
+    };
+
 
     render() {
-        return (
-            <Query query={GET_TASKS}>
-                {({ loading, error, data }) => {
-                    if (loading) {
-                        return <Loading />;
-                    }
-                    if (error) {
-                        return `Error! ${error.message}`;
-                    }
-                    const tasks: ITask[] = data.tasks;
-
-                    return <>
-                        <Grid container={true}>
-                            <Grid item={true} xs={true} style={{ padding: '2rem' }}>
-                                <Paper style={{ background: '#ffffff', padding: '1rem' }}>
-                                    <div><Typography variant="h6" component="h2">Nezapočaté</Typography></div>
-                                    <Divider style={{ marginTop: '.5rem', marginBottom: '.5rem' }} />
-
-                                    {tasks.map(x => x.state === State.Unresolved && <TaskCard task={x} key={x.id} />)}
-                                </Paper>
-                            </Grid>
-                            <Grid item={true} xs={true} style={{ padding: '2rem' }}>
-                                <Paper style={{ background: '#ffffff', padding: '1rem' }}>
-                                    <div><Typography variant="h6" component="h2">Započaté</Typography></div>
-                                    <Divider style={{ marginTop: '.5rem', marginBottom: '.5rem' }} />
-                                    {tasks.map(x => x.state === State.Unresolved && <TaskCard task={x} key={x.id} />)}
-                                </Paper>
-                            </Grid>
-                            <Grid item={true} xs={true} style={{ padding: '2rem' }}>
-                                <Paper style={{ background: '#ffffff', padding: '1rem' }}>
-                                    <div><Typography variant="h6" component="h2">Dokončené</Typography></div>
-                                    <Divider style={{ marginTop: '.5rem', marginBottom: '.5rem' }} />
-                                    {tasks.map(x => x.state === State.Solved && <TaskCard task={x} key={x.id} />)}
-                                </Paper>
-                            </Grid>
-                        </Grid>
-
-                    </>;
-                }}
-            </Query>
-        );
+        return <>
+            <Board data={this.tasksData} />
+        </>;
     }
 }
 
