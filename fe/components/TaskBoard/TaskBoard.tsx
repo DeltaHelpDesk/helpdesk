9426650139ed69d63f30b8cacc6e49f3a82bcb3f
Jan Kollert <koller.honzik@gmail.com>
2019-11-07 22:23:03 +0100

    Try no. 2

--- fe/components/TaskBoard/TaskBoard.tsx
+++ fe/components/TaskBoard/TaskBoard.tsx
@@ -10,46 +10,42 @@ import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
 import { tasksBoardQuery } from '../../src/graphql/queries';
 
+const Tasks = ({onTaskSelected}) => (
+    <Query query={tasksBoardQuery}>
+        {({ loading, error, data }) => {
+            if(loading) return Loading;
+            if(error) return 'Erohr! ${error.message}';
 
+            return (
+                <select name="task" onChange={onTaskSelected}>
+                    {data.tasks.map(task => (
+                        <option key={task.id} value={task.subject}>
+                            {task.issue}
+                        </option>
+                    ))}
+                </select>
+            );
+        }}
+    </Query>
+);
 
 class TaskBoard extends React.Component<{}> {
-
-    const { loading, error, data } = useQuery(tasksBoardQuery);
-
-    if(loading) return Loading;
-    if(error) return 'Errrrrrrohr! ${error.message}';
-
-    {data.tasks.map(task => (tasksData.lanes.cards.add(task)))}
-
     tasksData = {
         lanes: [
             {
                 id: 'uncompleted',
                 title: 'Nezapočato',
-                cards: [
-                    { id: 'TaskID', title: 'Write Blog', description: 'Can AI make memes', label: '15/10/2019'},
-                    { id: 'TaskID', title: 'Pay Rent', description: 'Transfer via NEFT', label: '15/10/2019'}
-                ]
+                cards: [Tasks]
             },
             {
                 id: 'inprogress',
                 title: 'Pracuje se na tom',
-                cards: [
-                    { id: 'TaskID', title: 'Buy car', description: 'Can AI make memes', label: '15/10/2019'},
-                    { id: 'TaskID', title: 'Rent new house', description: 'Transfer via NEFT', label: '10/10/2019'}
-                ]
+                cards: [Tasks]
             },
             {
                 id: 'completed',
                 title: 'Dokončeno',
-                cards: [
-                    { id: 'TaskID', title: 'Steal diamond', description: 'Can AI make memes', label: '15/10/1000'},
-                    { id: 'TaskID', title: 'Break window', description: 'Transfer via NEFT. Konečně se mi podařilo napsat nějaký text abych otestoval, jestli to umí více řádků', label: '14/05/2001'},
-                    { id: 'TaskID', title: 'Kill one person', description: 'Can AI make memes', label: '14/05/1000'},
-                    { id: 'TaskID', title: 'Stay alive', description: 'Zůstat živ ale jen tak na oko aby se neřeklo. Nevím co víc sem napsat aby to bylo na více řádků', label: '19/09/20019'},
-                    { id: 'TaskID', title: 'Nákup', description: 'Když budou mít mléko, tak jich vzít 5', label: '17/05/2008'},
-                    { id: 'TaskID', title: 'Nákup', description: 'Koupit 6 vajec', label: '13/01/2019'},
-                ]
+                cards: [Tasks]
             }
         ]
     };
