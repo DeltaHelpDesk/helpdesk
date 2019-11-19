9e66f98f002700dcc238026a199716294201b274
Jiri Falta <jirkafalta@gmail.com>
2019-11-19 15:09:29 +0100

    Test & Update

--- fe/components/Login/Login.tsx
+++ fe/components/Login/Login.tsx
@@ -251,7 +251,7 @@ const LoginPage: FunctionComponent<ILoginProps> = ({ showPassword, user: loginVa
                         <Grid item={true}>
                             <SocialButton appId="515939249183955"
                                 provider="facebook"
-                                scope="name,email"
+                                // scope="name,email"
                                 onLoginSuccess={facebookLoginSuccess}
                                 onLoginFailure={onExternalLoginFail}
                             >
