ca9d68b766e67c6d0afc25818837ebb0f4d2d3cc
Lossmeril <46922592+Lossmeril@users.noreply.github.com>
2019-12-04 12:32:32 +0100

    Login menu enhancement (#201)
    * Login menu enhancement
    
    * Social Media Buttons Fix
    
    * Comments

--- fe/components/Login/MicrosoftButtonLogin.tsx
+++ fe/components/Login/MicrosoftButtonLogin.tsx
@@ -7,26 +7,22 @@ import { mdiWindows } from "@mdi/js";
 
 interface IMicrosoftButtonLoginProps {
     onClick: () => void;
+    className?: string;
 }
 
-const MicrosoftButtonLogin: SFC<IMicrosoftButtonLoginProps> = ({ onClick }) => {
+const MicrosoftButtonLogin: SFC<IMicrosoftButtonLoginProps> = ({ onClick, className = null }) => {
     return (
-        <div>
-            <div>
-                <Button
-                    variant="contained"
-                    size="large"
-                    color="primary"
-                    onClick={onClick}
-                    style={{ width: "25rem" }}>
-                    <Icon path={mdiWindows}
-                        size={1}
-                        color="white"
-                    />
-                    {localisation.login.loginMicrosoft}
-                </Button>
-            </div>
-        </div>
+        <Button
+            variant="contained"
+            size="large"
+            color="primary"
+            onClick={onClick}
+            className={className}>
+            <Icon path={mdiWindows}
+                size={1}
+                color="white"
+            />
+        </Button>
     );
 };
 
