7fd791461b057c2271ff782b836f3b12a3612ce8
Jiri Falta <jirkafalta@gmail.com>
2020-01-21 21:55:35 +0100

    Schema updated

---
+++ fe/src/graphql/types/EditUser.ts
@@ -0,0 +1,20 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL mutation operation: EditUser
+// ====================================================
+
+export interface EditUser_editUser {
+  __typename: "User";
+  id: string;
+}
+
+export interface EditUser {
+  editUser: EditUser_editUser | null;
+}
+
+export interface EditUserVariables {
+  theme: string;
+}
