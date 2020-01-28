f678a23d5cec2cdd01ff7934df5a7be77a22750c
Jan Kollert <kollert.honzik@gmail.com>
2020-01-28 15:31:16 +0100

    Password change update

---
+++ fe/components/ChangePassword/LastPassword.tsx
@@ -0,0 +1,43 @@
+import { FunctionComponent, useState, FormEvent } from "react";
+import { IconButton, Dialog, DialogTitle, DialogContent, Grid, TextField, DialogActions, Button, InputAdornment } from "@material-ui/core";
+import EditOutlinedIcon from "@material-ui/icons/EditOutlined";
+import { useSnackbar } from "notistack";
+import localisation from "../../src/Locales/Localisations";
+
+interface ILoginProps {
+    showPassword: boolean;
+    user: IUser;
+}
+
+interface IUser {
+    password: string;
+}
+
+const LastPassword: FunctionComponent<ILoginProps> = ({ showPassword, user: loginVars }) => {
+
+    const [filled, setUser] = useState<IUser>(loginVars);
+
+    const handleInputChange = (e: FormEvent<HTMLInputElement>) => {
+        const property = e.currentTarget.name;
+        const value = e.currentTarget.value;
+
+        setUser({
+            ...filled,
+            [property]: value,
+        });
+    };
+
+    return <>
+        <TextField
+            id="filled[-adornment-password"
+            variant="filled"
+            name="password"
+            autoComplete="current-password"
+            label={localisation.login.password}
+            value={filled && filled.password || ""}
+            onChange={(e) => handleInputChange(e as FormEvent<HTMLInputElement>)}
+        />
+    </>;
+};
+
+export default LastPassword;
